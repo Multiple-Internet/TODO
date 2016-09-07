45ce10994537d44473cc6705d4107b442ff1db99
Kelly Wright <kelly.wright@kellyinnovation.com>
2016-09-07 14:07:15 -0400

    Add First Display.

--- display.py
+++ display.py
@@ -1,1 +1,5 @@
-import data
+class Display():
+	def display_tasks(self, tasks):
+		print("\n")
+		for task in tasks:
+			print(task)
--- todo.py
+++ todo.py
@@ -1,7 +1,13 @@
 from sys import exit
+import time
+from display import Display
 
 ### Defines Main Menu
 class TODO():
+	def __init__(self):
+		self.data = {}
+		self.displays = Display()
+
 	MAIN_MENU = (
 		"1: Display Tasks",
 		"2: Add Tasks",
@@ -9,35 +15,43 @@ class TODO():
 		"4: Remove Tasks",
 		"0: Exit Program",
 	)
+
 	#BEGIN DICTIONARY
-
-	data = {'task' : 'time'}
-
+	
 	
 	ADD_MENU = (
 		"1: Add a New Task",
 		"0: Return to Main Menu",
 		)
 
-
 	def add_tasks(self):
+		task_input = input("What do you need to do? \n >")
+		self.data[task_input] = time.time()
+		#example d['mynewkey'] = 'mynewvalue'
+
+
+	def add_tasks_menu(self):
 		while True:
-			menu_selection = self.get_menu_selection(self.MAIN_MENU)
+			menu_selection = self.get_menu_selection(self.ADD_MENU)
 
 			if menu_selection == "1":
-				pass
-			###### making menu navigation
+				self.add_tasks()
+			elif menu_selection == "0":
+				break
+			else:
+				display_selection_error(menu_selection)
+	
 
 
-
+	###### making menu navigation
 	def main_menu(self):
 		while True:
 			menu_selection = self.get_menu_selection(self.MAIN_MENU)
 
 			if menu_selection == "1":
-				pass
+				self.displays.display_tasks(self.data)
 			elif menu_selection == "2":
-				self.add_tasks()
+				self.add_tasks_menu()
 			elif menu_selection == "3":
 				pass
 			elif menu_selection == "4":
